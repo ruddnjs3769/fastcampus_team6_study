e5fc44ae3011d5e5df0e00827b5d9fb91e5d9984
KIMGYEONGWON <ruddnjs6651@gmail.com>
2023-03-24 21:09:16 +0900

    starbucks first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input')
+
+searchEl.addEventListener('click', function () {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+})
+
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+})
+
+console.log('첫번째 커밋');
