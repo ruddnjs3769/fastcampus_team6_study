aeb72c1100db018fd8f7ef897786314f69c9480f
KIMGYEONGWON <ruddnjs6651@gmail.com>
2023-03-28 16:46:33 +0900

    new branch commit

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ searchInputEl.addEventListener('blur', function () {
 })
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function () {
   console.log(window.scrollY);
@@ -25,6 +26,10 @@ window.addEventListener('scroll', _.throttle(function () {
     gsap.to(badgeEl, .6, {
       opacity: 0,
       display: 'none'
+    });
+    // 버튼 보이기
+    gsap.to(toTopEl, .2, {
+      x: 0
     })
   }
   else {
@@ -32,10 +37,21 @@ window.addEventListener('scroll', _.throttle(function () {
     gsap.to(badgeEl, .6, {
       opacity: 1,
       display: 'block'
+    });
+    //버튼 숨기기
+    gsap.to(toTopEl, .2, {
+      x: 100
     })
   }
 }, 300));
 // _.throttle(함수, 시간(ms))
+
+
+toTopEl.addEventListener('click', function() {
+  gsap.to(window, .7, {
+    scrollTo: 0
+  })
+});
 
 
 
@@ -73,6 +89,17 @@ new Swiper('.promotion .swiper-container', {
     nextEl: '.promotion .swiper-next'
   }
 });
+new Swiper('.awards .swiper-container', {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: ' .awards .swiper-next'
+  }
+});
+
 
 
 const promotionEl = document.querySelector('.promotion');
@@ -116,3 +143,20 @@ function floatingObject(selector, delay, size){
 floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
+
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+
+spyEls.forEach(function(spyEl) {
+  new ScrollMagic
+  .Scene({
+    triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+    triggerHook:.8, // 뷰 포트 위치 지정
+  })
+  .setClassToggle(spyEl, 'show')
+  .addTo(new ScrollMagic.Controller());
+});
+
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();//this year 출력
